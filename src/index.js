febd830ff998cc4048cb157ff12c10815f61fa03
derasan <fibonacci>
2018-12-12 06:13:18 +0400

    commit lp

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import jQuery from 'jquery';
+import popper from 'popper.js';
+import bootstrap from 'bootstrap';
+import WOW from 'wowjs';
+import style from "./scss/main.scss";
+
+const wow = new WOW.WOW().init();
